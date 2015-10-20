aa742095d9a9390d76d5ce029fe9017e042afc2a
joannegrace <joanne.mosuela@gmail.com>
2015-10-20 15:28:11 +0000

    completed section 4.8

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,30 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def new
+  # default: render 'new' template
+  end
+  
+  # in movies_controller.rb
+  def create
+  @movie = Movie.create!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+  end
+  
+  # in movies_controller.rb
+
+  def edit
+  @movie = Movie.find params[:id]
+  end
+
+  def update
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+  end
+end
